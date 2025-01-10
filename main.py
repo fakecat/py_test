76228affca384462599d62cdaf2213d5de56d4b9
fakecat <1246027803@qq.com>
2025-01-10 15:38:12 +0900

    version 1.1, add readline file list.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys, os, math
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QApplication, QWidget, QMessageBox, QHBoxLayout, QFileDialog, QAction, QScrollArea, QTextBrowser, QInputDialog, QLineEdit, QHeaderView
+from PyQt5.QtWidgets import QMenu, QApplication, QWidget, QMessageBox, QHBoxLayout, QFileDialog, QAction, QScrollArea, QTextBrowser, QInputDialog, QLineEdit, QHeaderView
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from subprocess import Popen, PIPE
@@ -20,18 +20,96 @@ def display_first_line(text):
     lines = text.split('\n')
     return lines[0] if lines else ''
 
-def traverse_directory(folder_path, folder_name):
-    top_item = QStandardItem(folder_name)
-    dir = QDir(folder_path)
-    file_list = dir.entryInfoList([])
-    for file in file_list:
-        file_item = QStandardItem(file.fileName())
-        if file.isDir():
-            traverse_directory(file.dir(), top_item)
+def add_items_from_folder(path, parent_item):
+    hidden_dirs = []
+    hidden_files = []
+    dirs = []
+    files = []
+
+    for entry in os.scandir(path):
+        if entry.name.startswith('.'):
+            if entry.is_dir():
+                hidden_dirs.append(entry)
+            elif entry.is_file():
+                hidden_files.append(entry)
         else:
-            child_item = QStandardItem(f"{file}")
-            top_item.appendRow(child_item)
-    return top_item
+            if entry.is_dir():
+                dirs.append(entry)
+            elif entry.is_file():
+                files.append(entry)
+
+    hidden_dirs = sorted(hidden_dirs, key=lambda x: x.name.lower())
+    dirs = sorted(dirs, key=lambda x: x.name.lower())
+
+    hidden_files = sorted(hidden_files, key=lambda x: x.name.lower())
+    files = sorted(files, key=lambda x: x.name.lower())
+
+    for hidden_dir in hidden_dirs:
+        hidden_dir_item = QStandardItem(hidden_dir.name)
+        hidden_dir_item.setEditable(False)
+        parent_item.appendRow(hidden_dir_item)
+        add_items_from_folder(hidden_dir.path, hidden_dir_item)
+
+    for hidden_file in hidden_files:
+        hidden_file_item = QStandardItem(hidden_file.name)
+        hidden_file_item.setEditable(False)
+        parent_item.appendRow(hidden_file_item)
+
+    for dir_entry in dirs:
+        dir_item = QStandardItem(dir_entry.name)
+        dir_item.setEditable(False)
+        parent_item.appendRow(dir_item)
+        add_items_from_folder(dir_entry.path, dir_item)
+
+    for file_entry in files:
+        file_item = QStandardItem(file_entry.name)
+        file_item.setEditable(False)
+        parent_item.appendRow(file_item)
+
+def show_context_menu(pos, tree, model):
+    index = tree.indexAt(pos)
+    item = model.itemFromIndex(index)
+
+    context_menu = QMenu()
+
+    if item:
+        if item.hasChildren():
+            open_action = QAction("Open Folder", tree)
+            open_action.triggered.connect(lambda: open_folder_action(item))
+            context_menu.addAction(open_action)
+        else:
+            edit_action = QAction("Edit File", tree)
+            edit_action.triggered.connect(lambda: edit_file_action(item))
+            context_menu.addAction(edit_action)
+
+        view_history_action = QAction("View Commit History", tree)
+        view_history_action.triggered.connect(lambda: view_commit_history_action(item))
+        context_menu.addAction(view_history_action)
+
+        delete_action = QAction("Delete", tree)
+        delete_action.triggered.connect(lambda: delete_item_action(item))
+        context_menu.addAction(delete_action)
+
+    context_menu.exec_(tree.mapToGlobal(pos))
+
+def open_folder_action(item):
+    print(f"Opening folder: {item.text()}")
+
+def edit_file_action(item):
+    print(f"Editing file: {item.text()}")
+
+def view_commit_history_action(item):
+    print(f"Viewing commit history for: {item.text()}")
+
+def delete_item_action(item):
+    print(f"Deleting item: {item.text()}")
+    parent = item.parent()
+    if parent:
+        reply = QMessageBox.question(None, 'Confirm Delete', f"Are you sure you want to delete '{item.text()}'?",
+                                     QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            parent.removeRow(item.row())
+            QMessageBox.information(None, 'Success', f"'{item.text()}' has been deleted.")
 
 def funcExecuteCommand():
     text, ok = QInputDialog.getText(None, 'Execute Command', 'Input Command:', QLineEdit.Normal, "")
@@ -95,24 +173,14 @@ def funcOpenFolder():
         return
     dir = QDir(folder_path)
     file_list = dir.entryList()
-    # print(len(file_list))
-    # for file in file_list:
-    #     print(file)
     gitTree = ui.gitTree
     model = QStandardItemModel()
     folder_name = folder_path.split('/')[-1]
+    print(f"folder_name : {folder_name}")
     top_item = QStandardItem(folder_name)
-    for file in file_list:
-        if file == ".":
-            continue
-        if file == "..":
-            continue
-        if QFileInfo(folder_path + f"/{file}").isDir:
-            top_item.appendRow(traverse_directory(folder_path + f"/{file}", file))
-    #     child_item = QStandardItem(f"{file}")
-    #     top_item.appendRow(child_item)
+    top_item.setEditable(False)
+    add_items_from_folder(folder_path, top_item)
     model.appendRow(top_item)
-    # model = traverse_directory(folder_path, top_item)
     gitTree.setModel(model)
     gitTree.header().setVisible(False)
     gitTree.setVerticalScrollBarPolicy(0)
@@ -120,6 +188,8 @@ def funcOpenFolder():
     gitTree.setHorizontalScrollBarPolicy(0)
     gitTree.setHorizontalScrollBar(ui.gitTreeHorizontalScrollBar)
     gitTree.itemDelegateForRow(0)
+    gitTree.setContextMenuPolicy(Qt.CustomContextMenu)
+    gitTree.customContextMenuRequested.connect(lambda pos: show_context_menu(pos, gitTree, model))
 
 def funcExit():
     print(f"exit...")
